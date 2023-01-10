1878ee35897f4b5f764b857978414f46d57a573a
Levi Lima <levil9490@gmail.com>
2023-01-10 09:13:07 -0300

    Adicionado scroll ao clicar nos itens de navegação

---
+++ main.js
@@ -0,0 +1,9 @@
+function abrirMenu() {
+    document.getElementById('menu-mobile').classList.remove('close');
+    document.getElementById('menu-mobile').classList.add('open');
+}
+
+function fecharMenu() {
+    document.getElementById('menu-mobile').classList.remove('open');
+    document.getElementById('menu-mobile').classList.add('close');
+}
